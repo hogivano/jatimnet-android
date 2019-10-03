3cdf6b64857e975eee90e234b88a03feae1d6f1c
Hendriyan Ogivano <ogivano06@gmail.com>
2019-10-03 18:54:33 +0700

    navigation bar

---
+++ constants/Colors.js
@@ -0,0 +1,14 @@
+const tintColor = '#00b7b7';
+
+export default {
+  tintColor,
+  tabIconDefault: '#ccc',
+  tabIconSelected: tintColor,
+  tabBar: '#fefefe',
+  errorBackground: '#f55858',
+  errorText: '#fff',
+  warningBackground: '#EAEB5E',
+  warningText: '#666804',
+  noticeBackground: tintColor,
+  noticeText: '#fff',
+};
